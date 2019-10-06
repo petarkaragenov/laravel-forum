e3eef17da710919068825695e69b6affed4d25e4
Petar Karagenov <petarkaragenov@gmail.com>
2019-10-06 18:54:23 +0200

    Added question form validation, posting question functionality and displaying message on success'

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Question;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
@@ -36,9 +37,11 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $request->user()->questions()->create($request->only('title', 'body'));
+
+        return redirect()->route('questions.index')->with('success', 'Your question has been submitted');
     }
 
     /**
@@ -49,7 +52,7 @@ class QuestionsController extends Controller
      */
     public function show(Question $question)
     {
-        //
+
     }
 
     /**
--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -19,21 +19,21 @@
                         @csrf
                         <div class="form-group">
                             <label for="question-title">Question Title</label>
-                            <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+                            <input type="text" name="title" value="{{ old('title') }}" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
                         
                             @if ($errors->has('title'))
                                 <div class="invalid-feedback">
-                                    <strong>{{ $errors->first('title') }}</strong>
+                                    <p>{{ $errors->first('title') }}</p>
                                 </div>
                             @endif
                         </div>
                         <div class="form-group">
                             <label for="question-body">Explain your question</label>
-                            <textarea name="body" id="body" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" row="10"></textarea>                        
+                            <textarea name="body" id="body" value="{{ old('body') }}" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}" row="10"></textarea>                        
                         
                             @if ($errors->has('body'))
                                 <div class="invalid-feedback">
-                                    <strong>{{ $errors->first('body') }}</strong>
+                                    <p>{{ $errors->first('body') }}</p>
                                 </div>
                             @endif
                         </div>
